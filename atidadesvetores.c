cea703a44c5226b71c5cf59f7178cebf1145ccf4
Davi Guedes <daviguedescontato17@gmail.com>
2025-05-30 10:00:56 -0300

    Add files via upload

---
+++ atidadesvetores.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+	
+	int nota1,nota2,media;
+	int mediafuncao;
+	
+
+   
+	
+	printf("Digite  a nota 1 do aluno: ");
+	scanf("%d", &nota1);
+	
+	printf("Digite  a nota 2 do aluno: ");
+	scanf("%d", &nota2);
+	
+	media = (nota1+nota2) /2;
+	
+	printf("Media do aluno =  %1.d\n",media);
+	
+	if (media >=5):
+	printf("Aluno Aprovado");
+	return 0;
+	
+	
+}
+	
