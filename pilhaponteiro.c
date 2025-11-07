5334809c4924960f6f3491711b7b421b64ac4f33
Davi Guedes <daviguedescontato17@gmail.com>
2025-11-07 10:07:05 -0300

    atualização do pilhaponteiroanterior

--- pilhaponteiro.c
+++ pilhaponteiro.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct pilha {
     int valor;
     struct pilha *prox;
 } PILHA;
 
-
 int main() {
- 	PILHA elemento1;
- 	elemento1.valor =10;
- 	elemento1.prox= NULL;
- 	
- 	PILHA elemento2;
- 	elemento2.valor = -5;
-	elemento2.prox = &elemento1;
-	
-	PILHA elemento3;
- 	elemento3.valor = 90;
-	elemento3.prox = &elemento2;
-	
-	printf("elemento1 = %d, elemento1 =%p,aponta prox= %p.\n",
-	elemento1.valor,&elemento1,elemento1.prox);
-	printf("elemento2 = %d, elemento2 =%p,aponta prox= %p.\n",
-	elemento2.valor,&elemento1,elemento2.prox);
-	printf("elemento3 = %d, elemento3 =%p,aponta prox= %p.\n",
-	elemento3.valor,&elemento3,elemento3.prox);
-	
+    PILHA *elemento1 = (PILHA*)malloc(sizeof(PILHA));
+    elemento1->valor = 10;
+    elemento1->prox = NULL;
+
+    PILHA *elemento2 = (PILHA*)malloc(sizeof(PILHA));
+    elemento2->valor = -5;
+    elemento2->prox = elemento1;
+
+    PILHA *elemento3 = (PILHA*)malloc(sizeof(PILHA));
+    elemento3->valor = 90;
+    elemento3->prox = elemento2;
+
+    printf("elemento1 = %d, elemento1 = %p, aponta prox = %p\n",
+            elemento1->valor, elemento1, elemento1->prox);
+
+    printf("elemento2 = %d, elemento2 = %p, aponta prox = %p\n",
+            elemento2->valor, elemento2, elemento2->prox);
+
+    printf("elemento3 = %d, elemento3 = %p, aponta prox = %p\n",
+            elemento3->valor, elemento3, elemento3->prox);
+
     return 0;
 }
 
